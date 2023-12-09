81b6e0af1c418850cbcb2737efaa1fd4984f4971
Alucardev <aguxten@gmail.com>
2023-12-08 23:06:34 -0300

    Solucion de problema basico, agregando funcion recursiva e implementando un algoritmo de busqueda binaria

---
+++ Startup.cs
@@ -0,0 +1,43 @@
+﻿using apipeliculas.Servicios;
+using Microsoft.EntityFrameworkCore;
+
+namespace apipeliculas;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddHttpContextAccessor();
+        services.AddAutoMapper(typeof(Startup));
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        services.AddControllers().AddNewtonsoftJson();
+        services.AddEndpointsApiExplorer();
+        services.AddScoped<IAlmacenadorArchivos, AlmacenadorArchivosLocal>();
+    }
+
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
